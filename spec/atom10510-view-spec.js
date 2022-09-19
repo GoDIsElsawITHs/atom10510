451e5d7def425c8734b87e685461f052bb483074
GoDIsElsawITHs <sawith2541@gmail.com>
2022-09-19 11:47:00 +0700

    atom10510

---
+++ spec/atom10510-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import Atom10510View from '../lib/atom10510-view';
+
+describe('Atom10510View', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
